0c4ee2f41c0a10ab1ffbe6f8e3ad377e875b1cf9
sunwoo030616 <sunwoo030616@dgu.ac.kr>
2026-07-25 23:36:11 +0900

    initial commit

---
+++ src/main/java/com/example/kb_ai_backend/KbAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kb_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kb_ai_backend/KbAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kb_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
